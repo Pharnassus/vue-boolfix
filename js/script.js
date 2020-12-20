a775d0ad8ed5a79b720a42ba3e728fe64de81b77
M <unreality@live.it>
2020-12-20 15:34:33 +0100

    problemi con la visualizzazione in input

--- js/script.js
+++ js/script.js
@@ -1,25 +1,69 @@
-/*
-Poster:
-L'API ci restituirà url relativo dell'immagine, come prefisso aggiungiamo:
-https://image.tmdb.org/t/p/w220_and_h330_face/ (formato: 220px X 330px)
-*/
+// "results": [
+//   {
+//     "adult": false,
+//     "backdrop_path": "/fq3wyOs1RHyz2yfzsb4sck7aWRG.jpg",
+//     "genre_ids": [
+//         12,
+//         35,
+//         878,
+//         10751
+//     ],
+//     "id": 105,
+//     "original_language": "en",
+//     "original_title": "Back to the Future",
+//     "overview": "Marty McFly è stato catapultato per errore nel 1955, grazie alla macchina del tempo ideata dal suo amico scienziato Doc. Non avendo più \"carburante\" per poter tornare nel futuro si rivolge alla versione più giovane di Doc, che nonostante l'incredulità iniziale si farà in quattro per aiutarlo. Ma nel 1955 non è solo Doc ad essere più giovane, Marty infatti si imbatte casualmente nei suoi genitori, all'epoca teenager, ma l'incontro aggiungerà altri problemi.",
+//     "popularity": 49.791,
+//     "poster_path": "/AkmUoSHkxW9txpzZ52gCcWweEkE.jpg",
+//     "release_date": "1985-07-03",
+//     "title": "Ritorno al futuro",
+//     "video": false,
+//     "vote_average": 8.3,
+//     "vote_count": 14221
+//   },
+// ]
+
+/* https://api.themoviedb.org/3/search/movie?
+api_key=bd77a6f388cd1f04e0f903f3b8a99556&
+query=ritorno+al+futuro&
+language=it-IT */
 
 var app = new Vue(
   {
-    el: '#root', 
+    el: '#root',
     data: {
+      cardFilms: [],
+      filmIndex: 0,
+      searchFilm: '',
     },
     methods: {
+      filterFilms: function() {
+        // console.log('filter');
+        this.cardFilms.forEach(
+          (item, i) => {
+            // console.log(item.title);
+            if (!item.title.toLowerCase().includes(this.searchFilm.toLowerCase())) {
+              item.poster_path = '';
+            }
+            // console.log(this.cardFilms);
+        });
 
+      }
     },
     mounted: function() {
 
       var self = this;
 
       axios
-        .get('https://flynn.boolean.careers/exercises/api/array/music')
-        .then( function(keiByGet) {
-
+        .get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: 'bd77a6f388cd1f04e0f903f3b8a99556',
+            query: 'fantozzi',
+            language: 'it-IT'
+          }
+        })
+        .then( function(element) {
+          self.cardFilms = element.data.results;
+          // console.log(self.cardFilms);
       });
     },
   },
